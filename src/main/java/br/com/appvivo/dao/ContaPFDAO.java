a356041c5a6ca9dff189fe4603301ffd85d42924
smk-1991 <samuel.lima1991@gmail.com>
2021-11-22 21:08:05 -0300

    refatoração classes ContaPF e PJ comparação de Strings por Enums

--- src/main/java/br/com/appvivo/dao/ContaPFDAO.java
+++ src/main/java/br/com/appvivo/dao/ContaPFDAO.java
@@ -3,22 +3,26 @@ package br.com.appvivo.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+
 import br.com.appvivo.entities.ContaPF;
 import br.com.appvivo.entities.UserPF;
 import br.com.appvivo.enuns.Produtos;
 
 public class ContaPFDAO extends GenericDAO<ContaPF, Integer> {
+	
+	Produtos produtos;
 
 	public ContaPFDAO(EntityManager em) {
 		super(em);
 	}
 
 	public List<UserPF> filtrarPorProduto(Produtos produtos) {
-		if (produtos.toString() == "TELEFONE_FIXO")
+		if (produtos.TELEFONE_FIXO == this.produtos.TELEFONE_FIXO) {
 			return em.createNativeQuery(
 					"SELECT tb_user_pf.id FROM conta_pf," + " tb_user_pf WHERE conta_pf.produtos = 'TELEFONE_FIXO' AND"
 							+ " conta_pf.id = tb_user_pf.conta_pf_id",
 					UserPF.class).getResultList();
+		}
 		else {
 			return em.createNativeQuery(
 					"SELECT tb_user_pf.id FROM conta_pf," + " tb_user_pf WHERE conta_pf.produtos <> 'TELEFONE_FIXO' AND"
--- src/main/java/br/com/appvivo/dao/ContaPJDAO.java
+++ src/main/java/br/com/appvivo/dao/ContaPJDAO.java
@@ -9,17 +9,20 @@ import br.com.appvivo.entities.UserPJ;
 import br.com.appvivo.enuns.Produtos;
 
 public class ContaPJDAO extends GenericDAO<ContaPJ, Integer> {
+	
+	Produtos produtos;
 
 	public ContaPJDAO(EntityManager em) {
 		super(em);
 	}
 
 	public List<UserPJ> filtrarPorProduto(Produtos produtos) {
-		if (produtos.toString() == "TELEFONE_FIXO")
+		if (produtos.TELEFONE_FIXO == this.produtos.TELEFONE_FIXO) {
 			return em.createNativeQuery(
 					"SELECT tb_user_pj.id FROM conta_pj," + " tb_user_pj WHERE conta_pj.produtos = 'TELEFONE_FIXO' AND"
 							+ " conta_pj.id = tb_user_pj.conta_pj_id",
 					UserPJ.class).getResultList();
+		}
 		else {
 			return em.createNativeQuery(
 					"SELECT tb_user_pj.id FROM conta_pj," + " tb_user_pj WHERE conta_pj.produtos <> 'TELEFONE_FIXO' AND"
